2ceae51cdb5564f84fad430b0684c45a76690dda
Henrique Gogó <henriquegogo@gmail.com>
2015-11-08 19:49:16 -0300

    Fixing rating belongs error

--- models/rating.js
+++ models/rating.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        Rating.belogsTo(models.Handyman);
+        Rating.belongsTo(models.Handyman);
       }
     }
   });
